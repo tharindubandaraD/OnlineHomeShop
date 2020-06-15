e01dc02f25e3b986beb55f4c3c9cfacb1e56e101
Tharindu Hasintha Bandara <40268850+tharinduHasinthaBandaraDissanayaka@users.noreply.github.com>
2020-06-15 12:59:17 +0530

    06/15/2020 - generate token after login

--- HomeShop.API/Controller/AuthController.cs
+++ HomeShop.API/Controller/AuthController.cs
@@ -1,40 +1,85 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using HomeShop.API.Data;
 using HomeShop.API.Dtos;
 using HomeShop.API.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HomeShop.API.Controller
 {
     [Route("api/[controller]")]
+    //without this the dont know from where request is comming from  - model state 
     [ApiController]
 
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
-        {          
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _config = config;
             _repo = repo;
         }
 
-         [HttpPost("register")]
+        [HttpPost("register")]
         //we are using DTO to parse into register method 
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDtos)
-        {            
+        {
             //validate request 
             userForRegisterDtos.UserName = userForRegisterDtos.UserName.ToLower();
 
             if (await _repo.UserExists(userForRegisterDtos.UserName))
                 return BadRequest("User already exisits");
-           
 
-            var userToCreate = new User 
+
+            var userToCreate = new User
             {
                 Username = userForRegisterDtos.UserName
             };
-            var createdUser = await _repo.Register(userToCreate, userForRegisterDtos.Password);        
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDtos.Password);
 
             return StatusCode(201);
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDtos userForLoginDtos)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDtos.Username.ToLower(), userForLoginDtos.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name , userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);           
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)               
+            });
 
         }
 
--- HomeShop.API/Dtos/UserForRegisterDto.cs
+++ HomeShop.API/Dtos/UserForRegisterDto.cs
@@ -1,10 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomeShop.API.Dtos
 {
     public class UserForRegisterDto
     {
+        //data annotations
+        [Required]
         public string UserName { get; set; }
+        
+        [Required]
+        [StringLength(8, MinimumLength= 4, ErrorMessage= "Password should be between 4 and 8 characters")]
         public string Password { get; set; }
         public string Gender { get; set; }
         public string KnownAs { get; set; }
