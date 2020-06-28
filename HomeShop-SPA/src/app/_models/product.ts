f4f1d329669db27cdce2acc1a7cbe1ba0700abb5
Tharindu Hasintha Bandara <40268850+tharinduHasinthaBandaraDissanayaka@users.noreply.github.com>
2020-06-28 18:12:25 +0530

    06-28-2020 Css

--- HomeShop-SPA/src/app/_models/product.ts
+++ HomeShop-SPA/src/app/_models/product.ts
@@ -2,13 +2,12 @@ import { Photo } from './Photo';
 
 export interface Product {
     id: number;
-    category: string;
     name: string;
     price: number;
     discount: number;
-    photoId: number;
     photoUrl: string;
     quantity?: number;
+    category?: string;
     description?: number;
     photos?: Photo[];
 }
